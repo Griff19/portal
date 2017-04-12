2e483f9b323c030f17cd6bf2ad05f2902d13a496
griff19 <griff19@mail.ru>
2017-04-12 10:06:39 +0800

    Большая переделка, создание каталога товаров, механизм загрузки и обработки изображений и др.

--- backend/views/orders/index_adm.php
+++ backend/views/orders/index_adm.php
@@ -16,48 +16,48 @@ $this->title = 'Заказы';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-    
+
 <div class="orders-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php 
-        //echo $this->render('_search', ['model' => $searchModel]); 
-        echo '<p>Вы можете просматривать все заказы.</p>';
+    <?php
+    //echo $this->render('_search', ['model' => $searchModel]);
+    echo '<p>Вы можете просматривать все заказы.</p>';
     ?>
 
     <p>
         <?= Html::a('Создать Заказ', ['create'], ['class' => 'btn btn-success']) ?>
         <?= Html::a('Выгрузить', ['dbtofile'], ['class' => 'btn btn-success']) ?>
         <?php
-        if($getFile){
+        if ($getFile) {
             echo '<a href="index.php?r=orders/download"> Скачать файл </a>';
         }
         ?>
     </p>
-    
-        
+
+
     <?php Pjax::begin(); ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
-        'rowOptions' => function ($model){
-            if ($model->status == 'Размещен'){
+        'rowOptions' => function ($model) {
+            if ($model->status == 'Размещен') {
                 return [
                     'class' => 'success',
 
-                    'onclick' => 'window.location.href="'.Url::toRoute(['orders/view', 'id' => $model->order_id]).'"; return false',
+                    'onclick' => 'window.location.href="' . Url::toRoute(['orders/view', 'id' => $model->order_id]) . '"; return false',
                     'style' => 'cursor:pointer'
                 ];
-            } elseif ($model->status == 'Черновик'){
+            } elseif ($model->status == 'Черновик') {
                 return [
                     'class' => 'danger',
-                    'onclick' => 'window.location.href="'.Url::toRoute(['orders/view', 'id' => $model->order_id]).'"; return false',
+                    'onclick' => 'window.location.href="' . Url::toRoute(['orders/view', 'id' => $model->order_id]) . '"; return false',
 
                     'style' => 'cursor:pointer'
                 ];
             } else {
-                return[
-                    'onclick' => 'window.location.href="'.Url::toRoute(['orders/view', 'id' => $model->order_id]).'"; return false',
+                return [
+                    'onclick' => 'window.location.href="' . Url::toRoute(['orders/view', 'id' => $model->order_id]) . '"; return false',
                     'style' => 'cursor:pointer'
                 ];
             }
@@ -77,11 +77,11 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
             [
                 'attribute' => 'order_amount',
-                'value' => function ($model){
+                'value' => function ($model) {
                     return $model->order_amount / 100;
                 }
             ],
-            
+
             //'user_id',
             'status',
 
@@ -89,26 +89,26 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]); ?>
     <?php Pjax::end(); ?>
-    <?php 
-        //тут я тренируюсь выгружать данные в файл
-        echo '<b> Выгружаемые Данные: </b><br>';
-        $order = new Orders;
-        $orders = $order->find()->where(['status' => 'Размещен'])->all();
-            
-        foreach($orders as $dt){
-            //print_r($dt);
-            $sm = new ListofgoodsSearch();
-            $dp = $sm->search(Yii::$app->request->queryParams,$dt['order_id']);
-                
-            foreach ($dp->models as $mod){
-                echo ' '.$dt['order_id'];
-                echo ' '.$dt['customersCustomer']['customer_name'];
-                echo ' '.$mod['goodsGoodId']['good_name'];
-                $good_price = $mod['goodsGoodId']['good_price'] / 100;
-                echo ' ' . $good_price;
-                echo '<br>';
-            }
-        };
+    <?php
+    //тут я тренируюсь выгружать данные в файл
+    echo '<b> Выгружаемые Данные: </b><br>';
+    $order = new Orders;
+    $orders = $order->find()->where(['status' => 'Размещен'])->all();
+
+    foreach ($orders as $dt) {
+        //print_r($dt);
+        $sm = new ListofgoodsSearch();
+        $dp = $sm->search(Yii::$app->request->queryParams, $dt['order_id']);
+
+        foreach ($dp->models as $mod) {
+            echo ' ' . $dt['order_id'];
+            echo ' ' . $dt['customersCustomer']['customer_name'];
+            echo ' ' . $mod['goodsGoodId']['good_name'];
+            $good_price = $mod['goodsGoodId']['good_price'] / 100;
+            echo ' ' . $good_price;
+            echo '<br>';
+        }
+    };
     ?>
 
 </div>
