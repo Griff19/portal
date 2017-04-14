df236e542afcccf21d908d790e5cb415322eaff7
griff19 <griff19@mail.ru>
2017-04-14 11:59:00 +0800

    каталог товаров разделение админской и пользовательской части

--- backend/controllers/GoodsController.php
+++ backend/controllers/GoodsController.php
@@ -158,6 +158,7 @@ class GoodsController extends Controller
     public function readfile()
     {
         ini_set('max_execution_time', 60);
+        ini_set('memory_limit', '150M');
         $filename = 'GoodsFile/GoodsPrice.txt';
         $readfile = fopen($filename, 'r');
         Goods::updateAll(['status' => 0]);
