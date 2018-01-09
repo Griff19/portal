7367089ba41b3618cea5e32fdc87aa27413a3cc9
griff19 <griff19@mail.ru>
2018-01-09 10:17:49 +0700

    тест страницы оператора на сервере 9 проверка логов

--- backend/helpers/Logs.php
+++ backend/helpers/Logs.php
@@ -17,24 +17,24 @@ class Logs
     const LOG_DIR = 'logs/log';
 
     public function add($str){
-//	    if (false) {
-//	    	echo $str . "\r\n";
-//		    $userIp = '127.0.0.1';
-//		    $userId = 'console';
-//		    $userName = 'console';
-//		    $fileLog = __DIR__ . '/../../backend/web/' . self::LOG_DIR;
-//	    } else {
+	    if (php_sapi_name() == 'cli') {
+	    	echo $str . "\r\n";
+		    $userIp = '127.0.0.1';
+		    $userId = 'console';
+		    $userName = 'console';
+		    $fileLog = __DIR__ . '/../../backend/web/' . self::LOG_DIR;
+	    } else {
 		    $userIp = Yii::$app->request->userIP;
 		    $userId = Yii::$app->user->id;
 		    $userName = User::getName($userId);
 		    $fileLog = self::LOG_DIR;
-	    //}
+	    }
 
         $date = new DateTime();
         $over = $date->format('Y-m-d H:i:s') . '; '.$userIp.'; ' . $userId . '; ' . $userName . '; ';
 
-        $w = fopen($fileLog, 'a');//открываем файл для записи в конец
-        fputs($w, $over . $str . "\r\n");//пишем строку в файл
-        fclose($w);  //закрываем файл
+        //$w = fopen($fileLog, 'a');//открываем файл для записи в конец
+        //fputs($w, $over . $str . "\r\n");//пишем строку в файл
+        //fclose($w);  //закрываем файл
     }
 }
