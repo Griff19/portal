e508cdc9606d9fb970afc439f533d0ebe4b6f9b2
griff19 <griff19@mail.ru>
2018-01-09 10:42:00 +0700

    тест страницы оператора на сервере 11 проверка логов

--- backend/helpers/Logs.php
+++ backend/helpers/Logs.php
@@ -17,15 +17,14 @@ class Logs
     const LOG_DIR = 'logs/log';
 
     public function add($str){
-	    var_dump(Yii::$app->request); die;
+
         if (php_sapi_name() == 'cli') {
             echo $str . "\r\n";
             $userIp = '127.0.0.1';
             $userId = 'console';
             $userName = 'console';
             $fileLog = __DIR__ . '/../../backend/web/' . self::LOG_DIR;
-        } elseif (Yii::$app->request) {
-	    } else {
+        } else {
 		    $userIp = Yii::$app->request->userIP;
 		    $userId = Yii::$app->user->id;
 		    $userName = User::getName($userId);
@@ -34,7 +33,7 @@ class Logs
 
         $date = new DateTime();
         $over = $date->format('Y-m-d H:i:s') . '; '.$userIp.'; ' . $userId . '; ' . $userName . '; ';
-
+        var_dump($fileLog); die;
         $w = fopen($fileLog, 'a');//открываем файл для записи в конец
         fputs($w, $over . $str . "\r\n");//пишем строку в файл
         fclose($w);  //закрываем файл
